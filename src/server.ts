258ade2ab4401f55ee9a5c0420ab5339a7fd58e5
ltg2004 <ltg@bbangyanetworks.com>
2020-10-27 02:32:27 +0900

    firstCommit

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hellow ts');
+});
+
+app.listen(3000, () => {
+    console.log('server start...');
+})
+console.log('hi')
